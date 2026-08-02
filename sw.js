1a4a831049f9e16b73acc47f04856d5e50ea6bf1
nyre-f <2212444051@qq.com>
2026-08-02 11:32:10 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='squirrel-life-v1-9-23-sokoban-hard-rebuild';
+const CACHE='squirrel-life-v1-9-25-pixel-rpg-ui';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-mark.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
